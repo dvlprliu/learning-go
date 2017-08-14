94db5d5d1572e7245bea51a0d48fdbde78c1207f
dvlprliu <dvlprliu@gmail.com>
2017-08-14 22:34:54 +0800

    rename

--- tempconv/tempconv.go
+++ tempconv/tempconv.go
@@ -17,8 +17,5 @@ const (
 	BoilingC      Celsius = 100
 )
 
-func CtoF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
-
-func (c Celsius) String() string { return fmt.Sprintf("%g˚C", c) }
+func (c Celsius) String() string    { return fmt.Sprintf("%g˚C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g˚F", f) }
