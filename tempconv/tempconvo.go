1ffea6b87793d69b64425bc396e6b6d926ab26ca
dvlprliu <dvlprliu@gmail.com>
2017-08-14 22:22:10 +0800

    add tempcon pkg

---
+++ tempconv/tempconvo.go
@@ -0,0 +1,24 @@
+// Package tempconv performs Celsius and Fahrenheit temperature computations.
+package tempconv
+
+import (
+	"fmt"
+)
+
+// Celsius type
+type Celsius float64
+
+// Fahrenheit type
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func CtoF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+func (c Celsius) String() string { return fmt.Sprintf("%g˚C", c) }
